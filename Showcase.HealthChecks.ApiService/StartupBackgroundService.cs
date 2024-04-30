b0a367155456655f96696b6e513de8e16e798b37
Colin McCullough <cmccullough@microsoft.com>
2024-04-30 16:54:51 -0400

    Add project files.

---
+++ Showcase.HealthChecks.ApiService/StartupBackgroundService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Showcase.HealthChecks.ApiService;
+
+public class StartupHealthCheck : IHealthCheck
+{
+    private volatile bool _isReady;
+
+    public bool StartupCompleted
+    {
+        get => _isReady;
+        set => _isReady = value;
+    }
+
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        if (StartupCompleted)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy("The startup task has completed."));
+        }
+
+        return Task.FromResult(HealthCheckResult.Unhealthy("That startup task is still running."));
+    }
+}
+
+public class StartupBackgroundService : BackgroundService
+{
+    private readonly StartupHealthCheck _healthCheck;
+
+    public StartupBackgroundService(StartupHealthCheck healthCheck)
+        => _healthCheck = healthCheck;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // Simulate the effect of a long-running task.
+        await Task.Delay(TimeSpan.FromSeconds(60), stoppingToken);
+
+        _healthCheck.StartupCompleted = true;
+    }
+}
